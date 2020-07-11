8adc5c83ec949fbbd2330fae5b587e0bcdd4bfe3
thisisaaronland <devnull@localhost>
2020-07-10 17:47:07 -0700

    update to newer reader.Reader interface

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/whosonfirst/go-reader-blob
 go 1.12
 
 require (
-	github.com/whosonfirst/go-reader v0.1.1
-	gocloud.dev v0.19.0
+	github.com/whosonfirst/go-reader v0.2.0
+	gocloud.dev v0.20.0
 )
--- reader.go
+++ reader.go
@@ -13,7 +13,7 @@ func init() {
 
 	for _, scheme := range blob.DefaultURLMux().BucketSchemes() {
 
-		err := wof_reader.RegisterReader(ctx, scheme, initializeBlobReader)
+		err := wof_reader.RegisterReader(ctx, scheme, NewBlobReader)
 
 		if err != nil {
 			panic(err)
@@ -21,39 +21,24 @@ func init() {
 	}
 }
 
-func initializeBlobReader(ctx context.Context, uri string) (wof_reader.Reader, error) {
-
-	r := NewBlobReader()
-	err := r.Open(ctx, uri)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return r, nil
-}
-
 type BlobReader struct {
 	wof_reader.Reader
 	bucket *blob.Bucket
 }
 
-func NewBlobReader() wof_reader.Reader {
-
-	r := BlobReader{}
-	return &r
-}
-
-func (r *BlobReader) Open(ctx context.Context, uri string) error {
+func NewBlobReader(ctx context.Context, uri string) (wof_reader.Reader, error) {
 
 	bucket, err := blob.OpenBucket(ctx, uri)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	r.bucket = bucket
-	return nil
+	r := &BlobReader{
+		bucket: bucket,
+	}
+
+	return r, nil
 }
 
 func (r *BlobReader) Read(ctx context.Context, uri string) (io.ReadCloser, error) {
